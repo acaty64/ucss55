35f9a5f1ef55a77cc91bec645750dc566763f539
acaty64 <ana.arashiro@gmail.com>
2017-12-24 19:59:44 +0000

    Vue JS components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('menu-component', require('./components/MenuComponent.vue'));
 
 const app = new Vue({
     el: '#app'
