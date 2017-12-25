6017180c5460278edd97458355056ea464c60ecf
acaty64 <ana.arashiro@gmail.com>
2017-12-25 01:48:22 +0000

    php artisan dusk

--- ci/setup_phpunit.sh
+++ ci/setup_phpunit.sh
@@ -11,8 +11,8 @@
     composer install --prefer-dist --no-interaction -o --optimize-autoloader
     
     php artisan key:generate    
-    php artisan migrate --force --env="ucss_tests"
-    php artisan db:seed --env="ucss_tests"
+    #php artisan migrate --force --env="ucss_tests"
+    #php artisan db:seed --env="ucss_tests"
 
     cp phpunit.xml.ci phpunit.xml
 
